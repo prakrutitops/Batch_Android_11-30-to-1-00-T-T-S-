d34307f127d37bc53fc25d201d279e2d5b085820
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2022-10-20 12:35:11 +0530

    Add files via upload

---
+++ Kotlin/20-10/DatatypeEx.kt
@@ -0,0 +1,21 @@
+fun main()
+{
+    //var a:Int=5
+
+    println("Enter your id")
+    var id = readLine()!!.toInt()
+
+    println("Enter your name")
+    var name= readLine()
+
+    println("Enter your salary")
+    var salary = readLine()!!.toDouble()
+
+
+    println("Your id is $id")
+    println("Your name is $name")
+    println("Your salary is $salary")
+
+
+
+}
